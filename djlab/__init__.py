f94a3f4004d48b0b2d2bc5edfd9e45aa659d5538
shenshan <shenshanpku@gmail.com>
2020-05-15 02:04:41 -0700

    Clean up

--- djlab/__init__.py
+++ djlab/__init__.py
@@ -4,33 +4,8 @@ import inspect
 from djutils.pipeline import Pipeline
 
 
-# ========================== HELPER METHODS =======================
+_lab_tables = (Lab, Location, User, UserRole, LabMembership,
+               ProtocolType, Protocol, Project, ProjectUser,
+               Source)
 
-_lab_tbls = (Lab, Location, User, UserRole, LabMembership,
-             ProtocolType, Protocol, Project, ProjectUser,
-             Source)
-
-pipeline = Pipeline(_lab_tbls)
-
-# def _init_lab_tbls(schema, context):
-
-#     init_tbls = {}
-#     for tbl in _lab_tbls:
-#         print(f'Initializing {tbl.__name__}')
-#         init_tbl = schema(tbl, context=context)
-#         init_tbls[tbl.__name__] = init_tbl
-
-#     return init_tbls
-
-
-# def init_lab_pipeline(schema, context={}, add_here=False):
-
-#     if add_here and not context:
-#         context = inspect.currentframe().f_back.f_locals
-
-#     lab_pipeline = _init_lab_tbls(schema, context)
-
-#     if add_here:
-#         context.update(**lab_pipeline)
-
-#     return lab_pipeline
+pipeline = Pipeline(_lab_tables)
