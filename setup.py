3f5b11c7603b690a5b9da4d6f37bfa252cb08b88
Thinh Nguyen <thinh@vathes.com>
2020-05-20 18:05:09 -0500

    slight rework based on new SchemaTemplate

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import sys
 here = path.abspath(path.dirname(__file__))
 
 long_description = """"
-Canonical DataJoint pipeline for lab management
+Canonical DataJoint for lab management
 """
 
 with open(path.join(here, 'requirements.txt')) as f:
